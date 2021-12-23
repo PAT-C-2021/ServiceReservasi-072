a326e4ce0c8001d97b1afb08276dd870b6ad03ec
anisanurfadila <anisanfd925@gmail.com>
2021-12-24 00:09:43 +0700

    praktikum11

--- ServiceReservasi/IService1.cs
+++ ServiceReservasi/IService1.cs
@@ -11,6 +11,21 @@ namespace ServiceReservasi
     [ServiceContract]
     public interface IService1
     {
+
+        [OperationContract]
+        string Login(string username, string password);
+        [OperationContract]
+        string Register(string username, string password, string kategori);
+        [OperationContract]
+        string UpdateRegister(string username, string password, string kategori, int id);
+        [OperationContract]
+        string DeleteRegister(string username);
+        [OperationContract]
+        List<DataRegister> DataRegist();
+
+
+
+
         [OperationContract]
         string pemesanan(string IDPemesanan, string NamaCustomer, string NoTelpon, int JumlahPemesanan, string IDLokasi);
         [OperationContract]
@@ -31,6 +46,19 @@ namespace ServiceReservasi
         CompositeType GetDataUsingDataContract(CompositeType composite);*/
 
         // TODO: Add your service operations here
+    }
+
+    [DataContract]
+    public class DataRegister
+    {
+        [DataMember(Order = 1)]
+        public int id { get; set; }
+        [DataMember(Order = 2)]
+        public string username { get; set; }
+        [DataMember(Order = 3)]
+        public string password { get; set; }
+        [DataMember(Order = 4)]
+        public string kategori { get; set; }
     }
 
     [DataContract]
